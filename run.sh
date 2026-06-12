1d43fb74beb7721b83b1bca973c5b05a6dd967ce
splettsova <splettspva@gmail.com>
2026-06-12 11:19:21 +0000

    Добавляю команды для данных в run.sh

--- run.sh
+++ run.sh
@@ -15,3 +15,23 @@ fi
 if [ "$1" = "run_reporter" ]; then
     docker run -v "$(pwd)/data:/data" reporter-image
 fi
+
+if [ "$1" = "create_local_data" ]; then
+    cp data/data.csv local_data/data.csv
+    cp data/report.html local_data/report.html
+fi
+
+if [ "$1" = "structure" ]; then
+    ls
+    ls generator
+    ls reporter
+    ls data
+    ls local_data
+fi
+
+if [ "$1" = "clear_data" ]; then
+    rm -f data/data.csv
+    rm -f data/report.html
+    rm -f local_data/data.csv
+    rm -f local_data/report.html
+fi
